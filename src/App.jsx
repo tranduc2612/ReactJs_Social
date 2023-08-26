98d81045e73e94b99c14e7c015486c7e6e618bba
Duc <mintduc2612@gmail.com>
2023-08-26 14:00:41 +0700

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { DefaultLayout } from "~/layouts/Index";
 import NotFound from "~/pages/notfound/Index";
 function App() {
   let isLogin = false;
+  let demo = false;
   return (
     <>
       <Router>
