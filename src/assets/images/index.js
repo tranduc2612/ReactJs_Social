00b0893f53403d451e3e29daf474076451709714
Duc <mintduc2612@gmail.com>
2023-08-29 00:57:30 +0700

    header middle

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,20 @@
 import img_logo from "./logo/facebook.svg";
 import search_icon from "./icon/search-icon.svg";
+import home_icon from "./icon/home-icon.svg";
+import youtube_icon from "./icon/youtube-icon.svg";
+import shop_icon from "./icon/shop-icon.svg";
+import group_icon from "./icon/group-icon.svg";
+import game_icon from "./icon/games-icon.svg"
 
 const images = {
   logo: img_logo,
   icon: {
-    search_icon
+    search_icon,
+    home_icon,
+    youtube_icon,
+    shop_icon,
+    group_icon,
+    game_icon
   }
 };
 
--- src/layouts/components/Header/Header.jsx
+++ src/layouts/components/Header/Header.jsx
@@ -2,10 +2,47 @@ import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
 import images from "~/assets/images/index";
 import Input from "~/components/Input/Input";
+import NavbarItem from "~/components/NavbarItem/NavbarItem";
+import { useState } from "react";
 const cx = classNames.bind(styles);
 
+const ListNav = [
+    {
+        id: 1,
+        title: "Trang chủ",
+        icon: images.icon.home_icon,
+        isActive: true
+    },
+    {
+        id: 2,
+        title: "Video",
+        icon: images.icon.youtube_icon,
+        isActive: false
+    },
+    {
+        id: 3,
+        title: "Marketplace",
+        icon: images.icon.shop_icon,
+        isActive: false
+    },
+    {
+        id: 4,
+        title: "Nhóm",
+        icon: images.icon.group_icon,
+        isActive: false
+    },
+    {
+        id: 5,
+        title: "Trò chơi",
+        icon: images.icon.game_icon,
+        isActive: false
+    }
+]
+
 function Header() {
+    const [listNav,setListNav] = useState(ListNav);
     return ( <div className={cx("header","d-flex align-items-center justify-content-between")}>
+            {/* Left */}
             <div className={cx("header__left","d-flex align-items-center justify-content-between")}>
                 <div className={cx("header__left-logo")}>
                     <img src={images.logo} />
@@ -14,10 +51,16 @@ function Header() {
                     <Input icon={images.icon.search_icon} />
                 </div>
             </div>
+            {/* Middle */}
             <div className={cx("header__middle")}>
-                middle
+                <ul className={cx("header__navbar","m-0 p-0")}>
+                    {listNav.map((nav)=>{
+                        return(<NavbarItem key={nav.id} isActive={nav.isActive} title={nav.title} icon={nav.icon} />)
+                    })}
+                </ul>
             </div>
-            <div className={cx("header__middle")}>
+            {/* Right */}
+            <div className={cx("header__right")} style={{width: "220px"}}>
                 right
             </div>
         </div> );
