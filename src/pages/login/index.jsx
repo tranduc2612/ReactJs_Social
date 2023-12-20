00c1e38be87a85c4ad506457a44a1fa5b038bca9
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-12-20 21:54:14 +0700

    deploy

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -45,7 +45,7 @@ function Login({ userData }) {
     const accountItem = () => (
         <div className={cx("account__item")}>
             <a className={cx("account__container")} href="">
-                <img src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-1/337055147_183576517474930_3019352573966962885_n.jpg?stp=dst-jpg_s480x480&_nc_cat=101&ccb=1-7&_nc_sid=5fac6f&_nc_ohc=5z2x-mjnMeMAX-_SGQ0&_nc_ht=scontent.fsgn2-4.fna&oh=00_AfAvLVrLTN0r0a4is_GWX5PRr5nR_fiNqpCfg4__SSizgQ&oe=64FB031B" alt="" />
+                <img src="https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-1/337055147_183576517474930_3019352573966962885_n.jpg?stp=dst-jpg_s480x480&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=U2-7z962joEAX-KuJQd&_nc_ht=scontent.fhan18-1.fna&oh=00_AfAFX5pmx8MKpnYTeVlfcUCL4l-MmBGhxIdVySMxtdpG8A&oe=6554005B" alt="" />
                 <div>Ngọc</div>
             </a>
             <a className={cx("remove_icon")} href="">x</a>
@@ -129,7 +129,7 @@ function Login({ userData }) {
                             </Formik>
                         </Box>
                         <div className={cx("bottom_text")}>
-                            <a href="#"> Tạo Trang </a> dành cho người nổi tiếng, thương hiệu hoặc doanh nghiệp.
+                            <a href="#"> Tạo Trang </a> dành cho người nổi tiếng, thương hiệu hoặc doanh nghiệp. Tài khoản test: admin, password: 123456
                         </div>
                     </div>
 
